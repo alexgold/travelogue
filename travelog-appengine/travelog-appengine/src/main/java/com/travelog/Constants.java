1ba29a15356e0f067ebe0e73c48601129ad60417
alexgold <goldshteinalexander@gmail.com>
2014-06-28 18:32:41 +0300

    first commit

---
+++ travelog-appengine/travelog-appengine/src/main/java/com/travelog/Constants.java
@@ -0,0 +1,13 @@
+package com.travelog;
+
+/**
+ * Contains the client IDs and scopes for allowed clients consuming the helloworld API.
+ */
+public class Constants {
+  public static final String WEB_CLIENT_ID = "523867230107-0q9qt3fue5et6pj9qeugjjqun48p0usj.apps.googleusercontent.com";
+  public static final String ANDROID_CLIENT_ID = "AIzaSyDis0J2D-iDHI0QJL9MSWPllC5Velz0vYw";
+  public static final String IOS_CLIENT_ID = "AIzaSyAXpbCOt0yMp9-xg2EfNQiaCOVRPugVVmE";
+  public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+
+  public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+}
